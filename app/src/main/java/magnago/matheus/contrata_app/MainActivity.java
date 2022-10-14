a1451ffb8b5ea0e147c9bc97c46b78b451c0f4f2
Matheus de Oliveira Magnago <magnagomatheus7@gmail.com>
2022-10-13 23:47:49 -0300

    navFragments - OK

--- app/src/main/java/magnago/matheus/contrata_app/MainActivity.java
+++ app/src/main/java/magnago/matheus/contrata_app/MainActivity.java
@@ -4,17 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import magnago.matheus.contrata_app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private ActivityMainBinding binding;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        initNavigation();
+
+    }
+
+    private void initNavigation() {
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
     }
 
 }
