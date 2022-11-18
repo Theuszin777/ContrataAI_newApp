37e2652975eec012d4347add0691440288f2b375
Matheus Magnago <magnagomatheus7@gmail.com>
2022-11-18 10:11:22 -0300

    Activity's new

--- app/src/main/java/magnago/matheus/contrata_app/fragments/MessagesFragment.java
+++ app/src/main/java/magnago/matheus/contrata_app/fragments/MessagesFragment.java
@@ -1,5 +1,6 @@
 package magnago.matheus.contrata_app.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,54 +8,56 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import magnago.matheus.contrata_app.ChatActivity;
 import magnago.matheus.contrata_app.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MessagesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MessagesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ImageButton imbMen1;
+    ImageButton imbMen2;
+    ImageButton imbMen3;
+    ImageButton imbMen4;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MessagesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MessagesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MessagesFragment newInstance(String param1, String param2) {
-        MessagesFragment fragment = new MessagesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        imbMen1.findViewById(R.id.imbMen1);
+        imbMen2.findViewById(R.id.imbMen2);
+        imbMen3.findViewById(R.id.imbMen3);
+        imbMen4.findViewById(R.id.imbMen4);
+
+        imbMen1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(MessagesFragment.this, ChatActivity.class);
+                startActivity(i1);
+            }
+        });
+
+        imbMen2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        imbMen3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        imbMen4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
