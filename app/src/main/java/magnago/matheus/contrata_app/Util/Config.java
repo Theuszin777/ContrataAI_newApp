9a5845c428e2d489aafc5f5e4fd5424d69ff579c
magnago <magnagomatheus7@gmail.com>
2022-12-10 15:58:10 -0300

    CONFIG

---
+++ app/src/main/java/magnago/matheus/contrata_app/Util/Config.java
@@ -0,0 +1,35 @@
+package magnago.matheus.contrata_app.Util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Config {
+
+    public static String CONTRATAI_URL = "";
+
+    public static void setEmail(Context context, String email) {
+        SharedPreferences mPrefs = context.getSharedPreferences("configs", 0);
+        SharedPreferences.Editor mEditor = mPrefs.edit();
+        mEditor.putString("email", email).apply();
+    }
+
+
+    public static String getEmail(Context context) {
+        SharedPreferences mPrefs = context.getSharedPreferences("configs", 0);
+        return mPrefs.getString("email", "");
+    }
+
+
+    public static void setPassword(Context context, String password) {
+        SharedPreferences mPrefs = context.getSharedPreferences("configs", 0);
+        SharedPreferences.Editor mEditor = mPrefs.edit();
+        mEditor.putString("password", password).apply();
+    }
+
+
+    /*public static String getPassword(Context context) {
+        SharedPreferences mPrefs = context.getSharedPreferences("configs", 0);
+        return mPrefs.getString("password", "");
+    }*/
+
+}
